b3ad84a0806ec105f83e613430ba7696d6d3da74
nedimon8691 <nedimon8691@gmail.com>
2021-08-03 13:47:00 +0600

    first commit

---
+++ testPostgreSQL/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using testPostgreSQL.Data;
+using testPostgreSQL.Models;
+
+namespace testPostgreSQL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public ApplicationDBContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDBContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IList<TestList> ListModel;
+        public IActionResult Index()
+        {
+            ListModel = _context.testLists.ToList();
+            return View(ListModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ testPostgreSQL/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testPostgreSQL.Models;
+
+namespace testPostgreSQL.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        public DbSet<TestList> testLists { get; set; }
+    }
+}
